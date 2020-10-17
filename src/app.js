356e6d3fea95028f603d5734ca0df5b4205dd55d
camilapessoa <camilabentespessoa@gmail.com>
2020-10-16 22:56:31 -0300

    add app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const router = require('../src/routes/gamesRoutes');
+
+app.use(express.json());
+app.use('/', router);
+
+module.exports = app;
